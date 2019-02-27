4336fc5dee2c8984bad6a52c50da8f9a0f5a39ab
twfraley <twfraley@gmail.com>
2019-02-27 09:59:46 -0500

    commit

---
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,30 @@
+﻿using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebApi.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+
+    }
+}
